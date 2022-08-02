a5ad5ded73bc44a564da89c3e889813789ff0e7d
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2022-08-03 00:23:21 +0700

    add file CardController

---
+++ src/main/java/edu/poly/controller/site/CartController.java
@@ -0,0 +1,45 @@
+package edu.poly.controller.site;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import edu.poly.domain.Product;
+import edu.poly.global.GlobalData;
+import edu.poly.service.ProductService;
+
+@Controller
+public class CartController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/cart")
+    public String cartGet(Model model){
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getUnitPrice).sum());
+        model.addAttribute("cart", GlobalData.cart);
+        return "cart";
+    }//page cart
+
+    @GetMapping("/addToCart/{id}")
+    public String addToCart(@PathVariable int id){
+        GlobalData.cart.add(productService.getProductById(id).get());
+        return "redirect:/shop";
+    }//click add from page viewProduct
+
+    @GetMapping("/cart/removeItem/{index}")
+    public String cartItemRemove(@PathVariable int index){
+        GlobalData.cart.remove(index);
+        return "redirect:/cart";
+    } // delete 1 product
+
+    @GetMapping("/checkout")
+    public String checkout(Model model){
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getUnitPrice).sum());
+        //model.addAttribute("cart", GlobalData.cart);
+        return "checkout";
+    } // checkout totalPrice
+}
