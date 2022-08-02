62b7587518b4f191d4d9947f77960b2a8e4dfa45
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2022-08-02 23:08:02 +0700

    disable default username and password for spring boot security

--- src/main/java/edu/poly/AssigmentsApplication.java
+++ src/main/java/edu/poly/AssigmentsApplication.java
@@ -3,13 +3,14 @@ package edu.poly;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import edu.poly.config.StorageProperties;
 import edu.poly.service.StorageService;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableConfigurationProperties(StorageProperties.class)
 public class AssigmentsApplication {
 
